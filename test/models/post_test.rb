1ac05f13a44a949741f7767585383e1535bbd045
Hollicint <hollyjdowling@gmail.com>
2024-11-16 21:10:13 +0000

    Adding test cases

---
+++ test/models/post_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class PostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "Should not save a post without a title" do
+    post = Post.new
+    assert_not post.save, "saved a post without a title"
+  end
+  test "Should  save a post with a title" do
+    post = Post.new(title: "Test")
+    assert post.save, "saved a post with a title"
+  end
+
+end
